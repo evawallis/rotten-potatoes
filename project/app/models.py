45d3e6092dccf1d7f6832bc8fe8ef4d05bc4ee00
evawallis <ew1466a@student.american.edu>
2024-04-09 15:50:18 +0200

    added artist, album, and song model classes

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+""" TODO:"""
+""" add classes user, user following relationship, and album artist relationship """
+
 class Album(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    name = models.CharField(max_length=30)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Artist(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):

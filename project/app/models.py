94d56897a9fddc6de06a33d9fdfcda3d07619452
evawallis <ew1466a@student.american.edu>
2024-04-16 17:26:34 +0200

    removed user class due to error

--- project/app/models.py
+++ project/app/models.py
@@ -25,15 +25,15 @@ class Song(models.Model):
     def __str__(self):
         return self.name
 
-class Person(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    follows = models.ManyToManyField("self")
+# class Person(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#     follows = models.ManyToManyField("self")
 
-    def __str__(self):
-        return self.user.username
+#     def __str__(self):
+#         return self.user.username
 
-class UserAlbumRating(models.Model):
-    user = models.ForeignKey(Person, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    rating = models.FloatField()
-    review = models.TextField()
+# class UserAlbumRating(models.Model):
+#     user = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+#     rating = models.FloatField()
+#     review = models.TextField()

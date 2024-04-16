46b687be2d39360a4af8cb29801bf378b30594ca
evawallis <ew1466a@student.american.edu>
2024-04-16 17:32:11 +0200

    fixing errors

--- project/users/models.py
+++ project/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    follows = models.ManyToManyField("self")
+
+    def __str__(self):
+        return self.user.username

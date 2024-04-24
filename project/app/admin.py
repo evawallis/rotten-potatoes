c1260214240092c369d10d7fc5c99844ad980ef7
Sam <sdwill@uvm.edu>
2024-04-24 17:36:24 +0200

    Edited Admin page to show IDs for artists and albums

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import *
 
+# Custom admin class
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'artist')
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
 # Register your models here.
-admin.site.register(Album)
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Song)
-admin.site.register(Artist)
+admin.site.register(Artist, ArtistAdmin)
 admin.site.register(UserAlbumRating)
